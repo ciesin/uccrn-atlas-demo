d5b685d6aad66b4284a950ffcc51a492e74cecae
George Verghese <gv2325@columbia.edu>
2025-03-13 12:39:43 -0400

    update vite config for base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 
 const config = {
+  base: process.env.BASE_URL || '/',
   build: {
     chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
   server: {
     open: true,
